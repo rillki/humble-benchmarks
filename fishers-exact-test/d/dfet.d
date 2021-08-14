1e118285fb0f562f685cb5fc4a3ab60627d3025a
John Colvin <john.loughran.colvin@gmail.com>
2021-08-14 11:17:53 +0100

    various improvements

--- fishers-exact-test/d/dfet.d
+++ fishers-exact-test/d/dfet.d
@@ -1,9 +1,16 @@
 module fisherExact;
 
 import std.stdio: writeln;
-import std.algorithm: sum;
-import std.math: log, exp;
+import std.algorithm: sum, min;
 import std.conv: to;
+
+// phobos std.math functions are very accurate but quite slow,
+// in my tests this makes a 2-3x speed difference overall
+version (phobosMath)
+	import std.math : log, exp;
+else version (libcMath)
+	import core.stdc.math : log, exp;
+else static assert(false, "Please set version phobosMath or libcMath");
 
 void main(string[] argv) {
 	// checking num of agruments
@@ -37,8 +44,9 @@ double[] logFactorial(const long n) {
 	return fs;
 }
 
-double logHypergeometricProbability(const long[] data, const ref double[] fs) {
-	return (
+pragma(inline, true)
+double logHypergeometricProbability(const long[] data, const double[] fs) {
+	return
 		fs[data[0] + data[1]] +
 		fs[data[2] + data[3]] +
 		fs[data[0] + data[2]] +
@@ -47,10 +55,15 @@ double logHypergeometricProbability(const long[] data, const ref double[] fs) {
 		fs[data[1]] -
 		fs[data[2]] -
 		fs[data[3]] -
-		fs[data[0] + data[1] + data[2] + data[3]]
-	);
+		fs[data[0] + data[1] + data[2] + data[3]];
 }
 
+// really need to do more than this pragma to make sure the optimizer doesn't cheat
+// and inline fisherExact in to main and then start using up-front knowledge of the
+// contingency table or see that they whole "loop n times" edifice doesn't change the
+// result, or something more subtle (pulling some calculations outside of the loop),
+// all of which will give misleading benchmark results.
+pragma(inline, false)
 double fisherExact(const long[] data) {
 	// sum all table values
 	const grandTotal = data.sum;
@@ -62,36 +75,20 @@ double fisherExact(const long[] data) {
 	const pvalThreshold = logHypergeometricProbability(data, factorials);
 
 	double pvalFraction = 0;
-	for(long i = 0; i <= grandTotal; i++) {
-		if((data[0] + data[1] - i >= 0) && (data[0] + data[2] - i >= 0) && (data[3] - data[0] + i >=0)) {
-			double lhgp = logHypergeometricProbability([
-				i,
-				data[0] + data[1] - i,
-				data[0] + data[2] - i,
-				data[3] - data[0] + i
-			], factorials);
+	foreach(i; data[3] - data[0] .. data[0] + min(data[1], data[2]) + 1) {
+		long[4] newData = [ // being explicit about not allocating
+			i,
+			data[0] + data[1] - i,
+			data[0] + data[2] - i,
+			data[3] - data[0] + i
+		];
+		double lhgp = logHypergeometricProbability(newData, factorials);
 
-			if(lhgp <= pvalThreshold) {
-				pvalFraction += exp(lhgp - pvalThreshold);
-			}
+		if(lhgp <= pvalThreshold) {
+			pvalFraction += exp(lhgp - pvalThreshold);
 		}
 	}
 
 	return exp(pvalThreshold + log(pvalFraction));
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
