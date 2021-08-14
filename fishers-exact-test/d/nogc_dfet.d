4bc84f379af53ab1fe41db79839d3fa1d40edb1b
ryuukk <44361234+ryuukk@users.noreply.github.com>
2021-08-14 12:05:32 +0200

    Here too

--- fishers-exact-test/d/nogc_dfet.d
+++ fishers-exact-test/d/nogc_dfet.d
@@ -42,7 +42,7 @@ Array!double logFactorial(const long n) {
 	return fs;
 }
 
-double logHypergeometricProbability(const long[4] data, const ref Array!double fs) {
+double logHypergeometricProbability(ref const long[4] data, const ref Array!double fs) {
 	return (
 		fs[data[0] + data[1]] +
 		fs[data[2] + data[3]] +
